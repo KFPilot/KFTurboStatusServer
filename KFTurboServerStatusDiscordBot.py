a9aa8453942009eb77a6c60b698025b9a6038fbf
KFPilot <pilotfromturnabout@gmail.com>
2026-03-12 23:28:57 -0400

    Increased duration of embed being stale before removed.

--- KFTurboServerStatusDiscordBot.py
+++ KFTurboServerStatusDiscordBot.py
@@ -373,7 +373,7 @@ async def update_session_embed(info: ServerPayload, session_id: str):
 
 async def delete_stale_embeds():
     global active_embeds
-    min_embed_age = datetime.datetime.now() - datetime.timedelta(minutes=5)
+    min_embed_age = datetime.datetime.now() - datetime.timedelta(minutes=15)
     to_delete = [sid for sid, v in active_embeds.items() if v.last_update < min_embed_age]
     for sid in to_delete:
         try:
